9ce8dd1429792696872096c24391e8137e4fcc4d
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2022-12-19 21:49:26 -0300

    feat: Criação do fundo cinza para os modais.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import Routes from "./router";
 import { GlobalStyle } from './styles/index';
 
 import "./App.css";
+import { ModalFundo } from "./components/ModalFundo/styled";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle>
-        <Routes />
-      </GlobalStyle/>
+       <Routes />
+      <GlobalStyle/>
     </div>
   );
 }
