8d8f4ec2c6b4061b509a1883d56bc50d8bae0af0
mateus <mateusbona28@gmail.com>
2022-12-22 15:48:17 -0300

    fix: ajustando modal

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -9,10 +9,6 @@ const Routes = () => {
       <Route path="/" exact>
         <Home />
       </Route>
-      <Route exact path="/">
-        <CardsList />
-        default page
-      </Route>
       <Route path="/login">login</Route>
       <Route path="/register">register</Route>
       <Route path="/dashboard">
