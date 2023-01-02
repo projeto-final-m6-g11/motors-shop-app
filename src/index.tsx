e5576dea4daf7451f63c4caf78d616a265354df1
Leonardo <yleeoembd@gmail.com>
2023-01-02 14:59:35 -0300

    feat: toastify container para alertas ao usuario

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AnnouncementsProvider } from "./contexts/announcements";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <BrowserRouter>
       <AnnouncementsProvider>
         <App />
