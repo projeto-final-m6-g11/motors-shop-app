003c7fbddc2e63d83818992e855f9e22d31ca915
Leonardo <yleeoembd@gmail.com>
2023-01-02 14:42:47 -0300

    feature Login sem funcionalidade no momento, porem todo componente funcional

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,64 @@
+import { useContext, useState } from "react";
+import API from "../../api";
+import Header from "../../components/header";
+import { FormStyled, Container } from "./styles";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const [inputUser, setInputUser] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  // const { setUser } = useContext(UserContext);
+  const sendData = () => {
+    if (inputUser && inputPassword !== "") {
+      const Login = new Promise((resolve, reject) =>
+        API.post("/login", {
+          username: inputUser,
+          password: inputPassword,
+        })
+          .then((res) => {
+            resolve(res);
+            // setUser(res.data)
+          })
+          .catch((err) => reject(err))
+      );
+
+      return toast.promise(Login, {
+        pending: "Acessando",
+        success: "Login efetuado",
+        error: "Usuário ou senha inválida",
+      });
+    }
+    return toast.warning("Preencha todos os campos corretamente");
+  };
+  return (
+    <>
+      <Header type="anonymous" />
+      <Container>
+        <FormStyled onSubmit={(e) => e.preventDefault()}>
+          <h1>Login</h1>
+          <label htmlFor="">Usuário</label>
+          <input
+            type="text"
+            placeholder="Digitar usuário"
+            required
+            onChange={(e) => setInputPassword(e.target.value)}
+          />
+          <label htmlFor="">Senha</label>
+          <input
+            type="password"
+            placeholder="Digitar senha"
+            required
+            onChange={(e) => setInputUser(e.target.value)}
+          />
+          <span className="forget__password">Esqueci minha senha</span>
+          <button className="enter__login" onClick={sendData}>
+            Entrar
+          </button>
+          <span className="create__account">Ainda não possui conta?</span>
+          <button onClick={() => {}}>Cadastrar</button>
+        </FormStyled>
+      </Container>
+    </>
+  );
+};
+export default Login;
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 25.75rem;
+  padding: 2.75rem 3rem;
+  gap: 0.625rem;
+  position: relative;
+  margin: 3.125rem 0rem;
+  h1 {
+    font-family: "Lexend";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.5rem;
+    line-height: 1.875rem;
+    margin-bottom: 0.625rem;
+  }
+  label {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 0.875rem;
+    line-height: 1.0625rem;
+    color: var(--grey1);
+
+  }
+  input {
+    border: 0;
+    width: 100%;
+    height: 2.5rem;
+    padding-left: 0.9375rem;
+    /* color: var(--grey3); */
+  }
+  button {
+    margin: 1rem 0rem;
+    width: 100%;
+    border: 0;
+    border-radius: 0.3125rem;
+    font-weight: 600;
+    font-family: Inter;
+    font-size: 1rem;
+    height: 3rem;
+    border: 0.0938rem solid var(--grey4);
+  }
+  .forget__password {
+    /* margin-top: 5px; */
+    display: flex;
+    justify-content: end;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 0.875rem;
+    line-height: 1.5rem;
+    color: var(--grey2);
+  }
+  .create__account {
+    display: flex;
+    justify-content: center;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 1.5rem;
+  }
+  .enter__login {
+    background-color: var(--brand1);
+    color: var(--whiteFixed);
+  }
+`;
+
+export const Container = styled.div`
+  width: 100vw;
+  /* height: 100%; */
+  display: flex;
+  justify-content: center;
+`;
